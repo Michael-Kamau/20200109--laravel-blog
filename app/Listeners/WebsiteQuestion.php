79136fd361ba35244b6e98cf8b5249b3a10247c1
Michael-Kamau <kamau.karitu@gmail.com>
2020-01-10 14:26:08 +0300

    Added error logging. Email failure is logged onto the log file with the data sent by the user.

--- app/Listeners/WebsiteQuestion.php
+++ app/Listeners/WebsiteQuestion.php
@@ -6,6 +6,9 @@ use App\Mail\RegisterMail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+use mysql_xdevapi\Exception;
+use function GuzzleHttp\Psr7\str;
 
 class WebsiteQuestion
 {
@@ -27,7 +30,14 @@ class WebsiteQuestion
      */
     public function handle($event)
     {
-        //Log::info('New Question From User.');
-        Mail::to('test@test.com')->send(new RegisterMail($event->data));
+        try{
+            Log::info('New Email Successfully sent');
+            Mail::to('test@test.com')->send(new RegisterMail($event->data));
+        }catch (\Exception $exception){
+            Log::info('Email failed to send');
+            Log::info('DATA SENT TO FAILED EMAIL: '.'Name=>'.$event->data['name'].' Question=>'.$event->data['question']);
+        }
+
+
     }
 }
